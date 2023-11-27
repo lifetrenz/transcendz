d9bdb7163c7919dde736d2d3379f2b86fadda0d3
जेरी एम.एस <jerry@lifetrenz.com>
2023-11-27 15:04:42 +0530

    add support for text body

--- src/Converter/ControllerArgumentResolver.php
+++ src/Converter/ControllerArgumentResolver.php
@@ -4,19 +4,30 @@ namespace Lifetrenz\Transcendz\Converter;
 
 use Lifetrenz\Transcendz\Attribute\JsonBody;
 use Lifetrenz\Transcendz\Attribute\QueryParams;
+use Lifetrenz\Transcendz\Attribute\TextBody;
 use ReflectionClass;
 
 class ControllerArgumentResolver
 {
-    public static function resolve(object|string $classObject, string $methodName, array $argumentList, ?string $jsonBody = null, array $queryParams = []): array
-    {
+    public static function resolve(
+        object|string $classObject,
+        string $methodName,
+        array $argumentList,
+        ?string $bodyContent = null,
+        array $queryParams = []
+    ): array {
         $controllerClassReflection = new ReflectionClass($classObject);
         $controllerMethodReflection = $controllerClassReflection->getMethod($methodName);
         $controllerMethodArguments = $controllerMethodReflection->getParameters();
         foreach ($controllerMethodArguments as $controllerArgument) {
             $jsonBodyAttribute = $controllerArgument->getAttributes(JsonBody::class)[0] ?? null;
             if ($jsonBodyAttribute !== null) {
-                $reqObject = $jsonBody === null ? null : JsonToObject::convert($jsonBody, $controllerArgument->getType()->getName());
+                $reqObject = $bodyContent === null ?
+                    null :
+                    JsonToObject::convert(
+                        $bodyContent,
+                        $controllerArgument->getType()->getName()
+                    );
                 $argumentList = array_map(
                     fn ($argument) =>
                     is_object($argument) && get_class($argument) === $controllerArgument->getType()->getName() ? $reqObject : $argument,
@@ -29,6 +40,13 @@ class ControllerArgumentResolver
                 $argumentList = array_map(
                     fn ($argument) =>
                     is_object($argument) && get_class($argument) === $controllerArgument->getType()->getName() ? $queryParamsObject : $argument,
+                    $argumentList
+                );
+            }
+            $textBodyAttribute = $controllerArgument->getAttributes(TextBody::class)[0] ?? null;
+            if ($textBodyAttribute !== null) {
+                $argumentList = array_map(
+                    fn ($argument) => is_string($argument) ? $bodyContent : $argument,
                     $argumentList
                 );
             }
