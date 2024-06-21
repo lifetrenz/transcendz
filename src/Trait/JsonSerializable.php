038294665156eb94ee758347ac5be98f45f97418
जेरी एम.एस <jerry@lifetrenz.com>
2024-06-21 16:35:09 +0530

    correct json serializable

--- src/Trait/JsonSerializable.php
+++ src/Trait/JsonSerializable.php
@@ -31,16 +31,21 @@ trait JsonSerializable
 
         $resultSet = array_diff_key(get_object_vars($this), array_flip($hiddenProps));
 
-        return array_map(
-            fn ($key, $var) => ($var instanceof DateTime) ?
-                (
-                    !array_key_exists($key, $jsonFormats) ?
-                    $var->getTimestamp() :
-                    $var->format($jsonFormats[$key])
-                ) :
-                $var,
-            array_keys($resultSet),
-            array_values($resultSet)
+        return array_merge(
+            ...array_map(
+                function ($key, $var) use ($jsonFormats) {
+                    if ($var instanceof DateTime) {
+                        if (array_key_exists($key, $jsonFormats)) {
+                            return [$key => $var->format($jsonFormats[$key])];
+                        }
+
+                        return [$key => $var->getTimestamp()];
+                    }
+                    return [$key => $var];
+                },
+                array_keys($resultSet),
+                array_values($resultSet)
+            )
         );
     }
 }
