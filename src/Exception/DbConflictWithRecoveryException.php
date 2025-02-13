05b25abdaad7b1c3ed06dc3d71f79ef6e20d072e
Jerry MS <jerry@lifetrenz.com>
2025-02-13 21:09:31 +0530

    add ConlictWithRecovery DB exception and remove generic Exception from Database.

---
+++ src/Exception/DbConflictWithRecoveryException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lifetrenz\Transcendz\Exception;
+
+use RuntimeException;
+
+final class DbConflictWithRecoveryException extends RuntimeException
+{
+}
--- src/PG/QueryExecutor.php
+++ src/PG/QueryExecutor.php
@@ -2,7 +2,7 @@
 
 namespace Lifetrenz\Transcendz\PG;
 
-use Exception;
+use Lifetrenz\Transcendz\Exception\DbConflictWithRecoveryException;
 use Lifetrenz\Transcendz\Exception\DbExecutionException;
 
 class QueryExecutor
@@ -17,37 +17,37 @@ class QueryExecutor
     public function execute()
     {
         $connection = $this->connection->connect();
-        try {
-            if ($this->setOptions !== null) {
-                pg_query($connection, $this->setOptions);
-            }
-            $result = pg_query($connection, $this->query);
-            if (!$result) {
-                throw new DbExecutionException(
+        if ($this->setOptions !== null) {
+            pg_query($connection, $this->setOptions);
+        }
+        $result = pg_query($connection, $this->query);
+        if (!$result) {
+            $error = pg_last_error($connection);
+            if (strpos($error, "canceling statement due to conflict with recovery") !== false) {
+                throw new DbConflictWithRecoveryException(
                     sprintf(
                         "Error in Executing DB function! \n Statement: %s \n Error: %s",
                         $this->query,
-                        pg_last_error($connection)
+                        $error
                     )
                 );
             }
-            return match (pg_num_rows($result)) {
-                -1 => throw new DbExecutionException(sprintf(
-                    "Error in Executing DB function! \n Statement: %s \n Error: %s",
-                    $this->query,
-                    pg_last_error($connection)
-                )),
-                0 => null,
-                default => pg_fetch_all($result)
-            };
-        } catch (Exception $ex) {
-            throw new Exception(
+            throw new DbExecutionException(
                 sprintf(
                     "Error in Executing DB function! \n Statement: %s \n Error: %s",
                     $this->query,
-                    pg_last_error($connection)
+                    $error
                 )
             );
         }
+        return match (pg_num_rows($result)) {
+            -1 => throw new DbExecutionException(sprintf(
+                "Error in Executing DB function! \n Statement: %s \n Error: %s",
+                $this->query,
+                pg_last_error($connection)
+            )),
+            0 => null,
+            default => pg_fetch_all($result)
+        };
     }
 }
